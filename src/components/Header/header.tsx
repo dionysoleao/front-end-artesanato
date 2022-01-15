a0b2fdd082283ad697a9234ada99d5471ce5448f
João Dionyso <dionysoleao@gmail.com>
2022-01-15 12:38:18 -0300

    adicionado mapa

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -11,7 +11,9 @@ export function Header() {
                 <Busca />
                 
             </div>
-            <img className={styles.mapa} src="/images/mapas.png" />
+            <div id="map" className={styles.mapa}></div>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly&channel=2" async></script>
+            <script src="./maps.js" async></script>
         </div>
     )
 }
