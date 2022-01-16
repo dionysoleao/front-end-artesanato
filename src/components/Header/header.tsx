b219006175d9bfb80d2ac9c13b124d621c8420d6
João Dionyso <dionysoleao@gmail.com>
2022-01-16 11:35:00 -0300

    incluido mapa

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,19 +1,14 @@
 import styles from './styles.module.scss'
 import {Menu} from './menu/menu'
 import { Busca } from './busca/busca'
-
-
 export function Header() {
     return (
         <div className={styles.header_container}>
             <div className={styles.header}>
                 <Menu />
                 <Busca />
-                
+                <iframe className={styles.mapa} src="https://api.mapbox.com/styles/v1/dionysoleao/ckydjqpfb6knv14ru3adduamv.html?title=false&access_token=pk.eyJ1IjoiZGlvbnlzb2xlYW8iLCJhIjoiY2t5ZGpuYWVtMDJwcDJ3cGg2cXlycTc4biJ9.x4wBIGutG9Bz3DvfmssDNw&zoomwheel=false#9.37/-2.2917/-44.7871" title="Basic" ></iframe>
             </div>
-            <div id="map" className={styles.mapa}></div>
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly&channel=2" async></script>
-            <script src="./maps.js" async></script>
         </div>
     )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
 import {Html, Head, Main, NextScript} from 'next/document'
 import Document from 'next/document'
 
+
+
 export default class myDocument extends Document {
     render(){
         return (
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
                     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" /> 
                 </Head>
                 <body>
